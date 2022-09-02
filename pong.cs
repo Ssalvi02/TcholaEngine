f99a5282d8f2aef1191e0b283f0776094aa268d9
Salvi <jaosalvi@protonmai.com>
2022-09-02 01:28:37 -0300

    Initial commit

---
+++ pong.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading;
+
+namespace pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Game loop
+            while (true)
+            {
+                Console.Clear();
+                Thread.Sleep(60);
+            }
+        }
+    }
+}
