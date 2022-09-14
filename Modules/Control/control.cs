ddca53d7094889883c995979761a19c1d2709995
Salvi <jaosalvi@protonmai.com>
2022-09-14 17:14:48 -0300

    Up

--- Modules/Control/control.cs
+++ Modules/Control/control.cs
@@ -4,5 +4,8 @@ using System.Text;
 
 namespace CTRL
 {
-    class Key { }
+    class Key
+    {
+
+    }
 }
--- pong.cs
+++ pong.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Threading;
 
-namespace pong
+namespace PONG
 {
     class Program
     {
