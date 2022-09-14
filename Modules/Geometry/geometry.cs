ad2c889250a861913f9d6589c3a40835a25b9872
Salvi <jaosalvi@protonmai.com>
2022-09-14 17:14:39 -0300

    Rect added

--- Modules/Geometry/geometry.cs
+++ Modules/Geometry/geometry.cs
@@ -1,10 +1,43 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
-namespace GT
+
+namespace GEOM
 {
-    class Triangle { }
-    class Rect { }
-    class Circle { }
+    public class Rect
+    {
+        public int H, B, PosX, PosY;
+        public char Symb;
+
+        public Rect(int b, int h, char symb, int posx, int posy)
+        {
+            H = h;
+            B = b;
+            PosX = posx;
+            PosY = posy;
+            Symb = symb;
+        }
+
+        public void Draw()
+        {
+            for (int y = PosY; y < PosY + B; y++)
+            {
+                for (int x = PosX; x < PosX + H; x++)
+                {
+                    PrintAtPosition(x, y, Symb);
+                }
+            }
+        }
+        static void PrintAtPosition(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
+        }
+    }
+    class Triangle
+    {
+
+    }
 }
