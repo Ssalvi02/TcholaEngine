c91049efd6d105ecc1adfdbac76366ea8060c502
sSalvi <jaosalvi@protonmail.com>
2022-09-15 23:30:53 -0300

    Update geometry.cs

--- Modules/Geometry/geometry.cs
+++ Modules/Geometry/geometry.cs
@@ -1,43 +1,52 @@
-using System;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
+
 
 
 namespace GEOM
 {
-    public class Rect
+    public class Shape
     {
-        public int H, B, PosX, PosY;
+        public int XSize, YSize, PosX, PosY;
         public char Symb;
 
-        public Rect(int b, int h, char symb, int posx, int posy)
-        {
-            H = h;
-            B = b;
-            PosX = posx;
-            PosY = posy;
-            Symb = symb;
-        }
-
-        public void Draw()
-        {
-            for (int y = PosY; y < PosY + B; y++)
-            {
-                for (int x = PosX; x < PosX + H; x++)
-                {
-                    PrintAtPosition(x, y, Symb);
-                }
-            }
-        }
         static void PrintAtPosition(int x, int y, char symb)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(symb);
         }
     }
-    class Triangle
+    public class Rect : Shape
     {
+        public Rect(int b, int h, char symb, int x, int y)
+        {
+            XSize = h;
+            YSize = b;
+            PosX = x;
+            PosY = y;
+            Symb = symb;
+        }
+        public void Draw()
+        {
+            for (int y = PosY; y < PosY + XSize; y++)
+            {
+                for (int x = PosX; x < PosX + YSize; x++)
+                {
+                    PrintAtPosition(x, y, Symb);
+                }
+            }
+        }
+    }
 
+    public class Triangle : Shape
+    {
+        public Triangle()
+        {
+
+        }
     }
 }
