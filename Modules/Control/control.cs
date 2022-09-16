2798129e2864fc902f7ed863f2877295342e8f48
Salvi <jaosalvi@protonmai.com>
2022-09-16 01:57:00 -0300

    Circulo não funciona direito

--- Modules/Control/control.cs
+++ Modules/Control/control.cs
@@ -4,8 +4,27 @@ using System.Text;
 
 namespace CTRL
 {
-    class Key
+    public class Sys
     {
+        static void RemoveScrollBars(int ForegroundColor)
+        {
+            // 0 = Black; 1 = DarkBlue; 2 = DarkGreen; 3 = DarkCyan; 4 = DarkRed;
+            // 5 = DarkMagenta; 6 = DarkYellow; 7 = Gray; 8 = DarkGray; 9 = Blue;
+            // 10 = Green; 11 = Cyan; 12 = Red; 13 = Magenta; 14 = Yellow; 15 = White;
+            
+            Console.ForegroundColor = ForegroundColor;
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
+        }
+
+        static int GetWindowH()
+        {
+            return Console.WindowHeight;
+        }
+        static int GetWindowW()
+        {
+            return Console.WindowWidth;
+        }
 
     }
 }
--- Modules/Geometry/geometry.cs
+++ Modules/Geometry/geometry.cs
@@ -14,12 +14,13 @@ namespace GEOM
         public int XSize, YSize, PosX, PosY;
         public char Symb;
 
-        static void PrintAtPosition(int x, int y, char symb)
+        public static void PrintAtPosition(int x, int y, char symb)
         {
             Console.SetCursorPosition(x, y);
             Console.Write(symb);
         }
     }
+
     public class Rect : Shape
     {
         public Rect(int b, int h, char symb, int x, int y)
@@ -47,6 +48,60 @@ namespace GEOM
         public Triangle()
         {
 
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public Circle(int radius, char symb, int x, int y)
+        {
+            XSize = radius;
+            PosX = x;   
+            PosY = y;
+            Symb = symb;
+        }
+
+        public void Draw()
+        {
+            switch (XSize)
+            {
+                case 0:
+                    PrintAtPosition(PosX, PosY, Symb);
+                    break;
+                default:
+                    double thickness = 0.1;
+                    Console.WriteLine();
+                    double rIn = XSize - thickness, rOut = XSize + thickness;
+
+                    for (double y = XSize; y >= -XSize; --y)
+                    {
+                        for (double x = -XSize; x < rOut; x += 0.5)
+                        {
+                            double value = x * x + y * y;
+                            if (value >= rIn * rIn && value <= rOut * rOut)
+                            {
+                                Console.Write(Symb);
+                            }
+                            else
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                        Console.WriteLine();
+                    }
+                    break;
+            }
+        } 
+    }
+
+    class prog
+    {
+        static void Main()
+        {
+            Circle c = new Circle(2, '*', 80, 5);
+
+            Console.Clear();
+            c.Draw();
         }
     }
 }
