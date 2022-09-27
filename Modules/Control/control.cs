9f7bee2f8081e688e1c478275e3a6e6566cdc7b4
sSalvi <jaosalvi@protonmail.com>
2022-09-27 17:06:48 -0300

    Input added

--- Modules/Control/control.cs
+++ Modules/Control/control.cs
@@ -10,22 +10,22 @@ namespace CONTROL
 {
     public class Sys
     {
-        static void RemoveScrollBars(System.ConsoleColor FC)
+        public static void RemoveScrollBars(int FC)
         {
             // 0 = Black; 1 = DarkBlue; 2 = DarkGreen; 3 = DarkCyan; 4 = DarkRed;
             // 5 = DarkMagenta; 6 = DarkYellow; 7 = Gray; 8 = DarkGray; 9 = Blue;
             // 10 = Green; 11 = Cyan; 12 = Red; 13 = Magenta; 14 = Yellow; 15 = White;
-            
-            Console.ForegroundColor = FC;
-            Console.BufferHeight = Console.WindowHeight;
-            Console.BufferWidth = Console.WindowWidth;
+            System.ConsoleColor c = (System.ConsoleColor)FC;
+            Console.ForegroundColor = c;
+            Console.BufferHeight = GetWindowH();
+            Console.BufferWidth = GetWindowW();
         }
 
-        static int GetWindowH()
+        public static int GetWindowH()
         {
             return Console.WindowHeight;
         }
-        static int GetWindowW()
+        public static int GetWindowW()
         {
             return Console.WindowWidth;
         }
@@ -35,7 +35,7 @@ namespace CONTROL
     public enum KeyCode
     {
         A = 'A', B = 'B', C = 'C', D = 'D', E = 'E', F = 'F', G = 'G', H = 'H', I = 'I', J = 'J', K = 'K', L = 'L', M = 'M', N = 'N', O = 'O', P = 'P', Q = 'Q', R = 'R', S = 'S', T = 'T', U = 'U', V = 'V', W = 'W', X = 'X', Y = 'Y', Z = 'Z', 
-        Space = 32, LeftCtrl, RightCtrl, LeftShift, RightShift, Enter, Backspace, Escape = 27, RightArrow, LeftArrow, UpArrow, DownArrow
+        Space = 32, Escape = 27
     }
     public class Input
     {
@@ -46,32 +46,6 @@ namespace CONTROL
                 return true;
             }
             return false;
-        }
-    }
-
-    class Program
-    {
-        static volatile bool exit = false;
-        static void Main(string[] args)
-        {
-            Task.Factory.StartNew(() =>
-                    {
-                        while (Console.ReadKey().Key != ConsoleKey.Q);
-                        exit = true;
-                    });
-
-                while (!exit)
-                {
-                    if(Input.GetKey(KeyCode.A))
-                    {
-                        Console.Write("JAJAjAJA");
-                    }
-                    if(Input.GetKey(KeyCode.B))
-                    {
-                        Console.Write("JUJUJUJUJ");
-                    }
-                    Thread.Sleep(60);
-                }
         }
     }
 }
