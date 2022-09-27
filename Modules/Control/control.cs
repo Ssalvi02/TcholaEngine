455d4811889e5d48c53d0954c6d0f17ed56dcba2
sSalvi <jaosalvi@protonmail.com>
2022-09-27 13:28:06 -0300

    Control added

--- Modules/Control/control.cs
+++ Modules/Control/control.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace CTRL
+
+//Problemas: Assincorinicidade de pegar input e como pegar caracteres especiais
+namespace CONTROL
 {
     public class Sys
     {
-        static void RemoveScrollBars(int ForegroundColor)
+        static void RemoveScrollBars(System.ConsoleColor FC)
         {
             // 0 = Black; 1 = DarkBlue; 2 = DarkGreen; 3 = DarkCyan; 4 = DarkRed;
             // 5 = DarkMagenta; 6 = DarkYellow; 7 = Gray; 8 = DarkGray; 9 = Blue;
             // 10 = Green; 11 = Cyan; 12 = Red; 13 = Magenta; 14 = Yellow; 15 = White;
             
-            Console.ForegroundColor = ForegroundColor;
+            Console.ForegroundColor = FC;
             Console.BufferHeight = Console.WindowHeight;
             Console.BufferWidth = Console.WindowWidth;
         }
@@ -26,5 +30,48 @@ namespace CTRL
             return Console.WindowWidth;
         }
 
+    }
+
+    public enum KeyCode
+    {
+        A = 'A', B = 'B', C = 'C', D = 'D', E = 'E', F = 'F', G = 'G', H = 'H', I = 'I', J = 'J', K = 'K', L = 'L', M = 'M', N = 'N', O = 'O', P = 'P', Q = 'Q', R = 'R', S = 'S', T = 'T', U = 'U', V = 'V', W = 'W', X = 'X', Y = 'Y', Z = 'Z', 
+        Space = 32, LeftCtrl, RightCtrl, LeftShift, RightShift, Enter, Backspace, Escape = 27, RightArrow, LeftArrow, UpArrow, DownArrow
+    }
+    public class Input
+    {
+        public static bool GetKey(KeyCode k)
+        {
+            if(Console.ReadKey().Key == ((ConsoleKey)k))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static volatile bool exit = false;
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() =>
+                    {
+                        while (Console.ReadKey().Key != ConsoleKey.Q);
+                        exit = true;
+                    });
+
+                while (!exit)
+                {
+                    if(Input.GetKey(KeyCode.A))
+                    {
+                        Console.Write("JAJAjAJA");
+                    }
+                    if(Input.GetKey(KeyCode.B))
+                    {
+                        Console.Write("JUJUJUJUJ");
+                    }
+                    Thread.Sleep(60);
+                }
+        }
     }
 }
